c0ffbbbf05c65cdeb75428e1460c3c7efe9d8b77
Francisco De La Cruz <helloquico@gmail.com>
2023-12-14 21:05:59 +0100

    feat(lir): update mappings

--- lua/plugins/lir.lua
+++ lua/plugins/lir.lua
@@ -21,12 +21,12 @@ return {
           ["h"] = actions.up,
           ["q"] = actions.quit,
 
-          ["K"] = actions.mkdir,
-          ["N"] = actions.newfile,
-          ["R"] = actions.rename,
+          ["A"] = actions.mkdir,
+          ["a"] = actions.newfile,
+          ["r"] = actions.rename,
           ["@"] = actions.cd,
           ["Y"] = actions.yank_path,
-          ["."] = actions.toggle_show_hidden,
+          ["i"] = actions.toggle_show_hidden,
           ["D"] = actions.delete,
 
           ["J"] = function()
