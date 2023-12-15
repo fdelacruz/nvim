5b9f9407b3b583a745ff8250c9d58f0b320a65c8
Francisco De La Cruz <helloquico@gmail.com>
2023-12-15 14:19:42 +0100

    feat(neo-tree): re-enable netrw hijacking

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,8 +1,8 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    filesystem = {
-      hijack_netrw_behavior = "disabled",
-    },
+    -- filesystem = {
+    --   hijack_netrw_behavior = "disabled",
+    -- },
   },
 }
