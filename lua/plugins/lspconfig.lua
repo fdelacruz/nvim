549736c2b25af36379df5f66fef18875a552efc9
Francisco De La Cruz <helloquico@gmail.com>
2023-12-15 18:06:11 +0100

    feat(tsserver): enable inlay hints

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -14,6 +14,23 @@ return {
             },
           },
         },
+        tsserver = {
+          single_file_support = false,
+          settings = {
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
       },
     },
   },
