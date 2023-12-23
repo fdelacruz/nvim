262d704ff7a3881737e6c26b1b56f54e2bb51662
Francisco De La Cruz <helloquico@gmail.com>
2023-12-23 15:11:10 +0100

    revert(lualine): remove lsp and formatters

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,43 +1,5 @@
 local Util = require("lazyvim.util")
 
-local hide_in_width = function()
-  return vim.o.columns > 80
-end
-
-local lsp = {
-  function()
-    local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
-    if #buf_clients == 0 then
-      return "LSP Inactive"
-    end
-
-    local formatters = require("conform").list_formatters()
-
-    local buf_client_names = {}
-    local buf_formatters = {}
-
-    -- add client
-    for _, client in pairs(buf_clients) do
-      if client.name ~= "null-ls" then
-        table.insert(buf_client_names, client.name)
-      end
-    end
-
-    -- add formatter
-    for _, formatter in pairs(formatters) do
-      table.insert(buf_formatters, formatter.name)
-    end
-
-    vim.list_extend(buf_client_names, buf_formatters)
-
-    local unique_client_names = table.concat(buf_client_names, ", ")
-    local language_servers = string.format("[%s]", unique_client_names)
-
-    return language_servers
-  end,
-  color = { gui = "bold" },
-  cond = hide_in_width,
-}
 
 local function env_cleanup(venv)
   if string.find(venv, "/") then
@@ -88,17 +50,6 @@ return {
         { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
         { Util.lualine.pretty_path() },
         python_env,
-      },
-      lualine_x = {
-        lsp,
-        {
-          "diff",
-          symbols = {
-            added = icons.git.added,
-            modified = icons.git.modified,
-            removed = icons.git.removed,
-          },
-        },
       },
       lualine_y = {
         "location",
