c2c47cab49dc5b7be4cc96e8268413dfad0fdce3
Francisco De La Cruz <helloquico@gmail.com>
2024-02-28 15:05:10 +0530

    feat(conform): setup eslint for javascript

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
-      ["javascript"] = { "prettier" },
+      ["javascript"] = { "eslint" },
       ["javascriptreact"] = { "prettier" },
     },
   },
