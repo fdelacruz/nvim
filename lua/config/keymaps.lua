73014abb5d6dd6133be11774c9475c0d8a6a39f8
Francisco De La Cruz <helloquico@gmail.com>
2024-08-01 22:57:27 +0200

    feat: add nvim-tmux-navigator
    override LazyVim <C-hjkl> keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,10 @@ local keymap = vim.keymap.set
 
 keymap("n", "-", ":lua require'lir.float'.toggle()<CR>", { noremap = true, silent = true, desc = "Open lir" })
 keymap("n", ";", ":Dashboard<CR>", { noremap = true, silent = true, desc = "Open Dashboard" })
+
+keymap("n", "<C-h>", ":NvimTmuxNavigateLeft<CR>", { silent = true, desc = "Navigate Left" })
+keymap("n", "<C-j>", ":NvimTmuxNavigateDown<CR>", { silent = true, desc = "Navigate Down" })
+keymap("n", "<C-k>", ":NvimTmuxNavigateUp<CR>", { silent = true, desc = "Navigate UP" })
+keymap("n", "<C-l>", ":NvimTmuxNavigateRight<CR>", { silent = true, desc = "Navigate Right" })
+keymap("n", "<C-\\>", ":NvimTmuxNavigateLastActive<CR>", { silent = true, desc = "Navigate Last Active" })
+keymap("n", "<C-Space>", ":NvimTmuxNavigateNext<CR>", { silent = true, desc = "Navigate Next" })
