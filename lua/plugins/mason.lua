fcca4b85ce1ed5b5e001c481efdb535554324ef5
Francisco De La Cruz <helloquico@gmail.com>
2025-09-02 19:43:25 +0200

    chore(mason): auto install LSPs

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,28 @@
 return {
   "williamboman/mason.nvim",
-  opts = { ui = { border = "rounded" } },
+  opts = {
+    ui = { border = "rounded" },
+    ensure_installed = {
+      "ruff",
+      "pyright",
+      "black",
+
+      "emmet-ls",
+      "html-lsp",
+      "htmx-lsp",
+      "css-lsp",
+      "typescript-language-server",
+      "eslint-lsp",
+      "prettier",
+      "jinja-lsp",
+
+      "lua-language-server",
+      "stylua",
+
+      "shfmt",
+      "shellcheck",
+
+      "taplo",
+    },
+  },
 }
