087f59351e50abc6f1a762921a2d550efcb06184
Francisco De La Cruz <helloquico@gmail.com>
2025-09-06 16:13:27 +0200

    feat(blink-cmp): add border to completion and documentation

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = function(_, opts)
+    opts.completion = {
+      menu = {
+        border = "single",
+      },
+      documentation = {
+        auto_show = true,
+        window = {
+          border = "single",
+        },
+      },
+    }
+  end,
+}
