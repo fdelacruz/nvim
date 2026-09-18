49a5f382bc6a6d47fe3f8c5c9aeedc5314931681
Francisco De La Cruz <helloquico@gmail.com>
2026-09-18 18:29:09 +0200

    feat(venv-selector): enable cached venv auto-activation and notifications

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -2,8 +2,8 @@ return {
   "linux-cultist/venv-selector.nvim",
   opts = {
     options = {
-      notify_user_on_venv_activation = false,
-      cached_venv_automatic_activation = false,
+      notify_user_on_venv_activation = true,
+      cached_venv_automatic_activation = true,
     },
   },
 }
